9a163c70f7fead62f45b9e7c67b4e859758247f5
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-07-19 16:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (41.15%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> v(26,-1);
+       vector<bool> vis(26,false);
+       for(int i = 0; i < s.size(); i++){
+           v[s[i]-'a'] = i;
+       } 
+       stack<char> st;
+       for(int i = 0; i < s.size(); i++){
+           if(vis[s[i]-'a']) continue; 
+           while(!st.empty() && s[i] < st.top() && v[st.top()-'a'] > i){
+               vis[st.top()-'a'] = false; 
+               st.pop();
+           }
+           st.push(s[i]);
+           vis[s[i]-'a'] = true;
+       }
+       
+       string ans;
+       while(!st.empty()){
+           ans += st.top();
+           st.pop();
+       }
+       reverse(ans.begin(), ans.end());
+       return ans;
+    }
+};
