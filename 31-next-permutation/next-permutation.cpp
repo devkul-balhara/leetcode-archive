75576ba0c426bf67de37fafcc1b814bb66c1ab83
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-08-01 04:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (77.21%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,7 +3,7 @@ public:
     void nextPermutation(vector<int>& nums) {
 
         // Step 1: Find the first index from the right where
-        // nums[idx] < nums[idx + 1].
+        // nums[i] > nums[i - 1].
         // This is the pivot that needs to be increased.
         int idx = -1;
         for (int i = nums.size() - 1; i > 0; i--) {
@@ -17,7 +17,8 @@ public:
         // (largest permutation). Reverse to get the smallest permutation.
         if (idx == -1) {
             reverse(nums.begin(), nums.end());
-        } else {
+        } 
+        else {
 
             // Step 2: Find the smallest element greater than nums[idx].
             // Since the suffix is in descending order, the first greater
