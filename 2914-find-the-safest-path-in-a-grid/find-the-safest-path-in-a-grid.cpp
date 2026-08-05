021b95020969e10322d810626f801051b04bbbb6
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-08-05 17:38:06 +0530

    Time: 256 ms (90.24%) | Memory: 128.6 MB (73.19%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return 0;
+        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return 0;
+        // bfs to find min manhattan distance of each cell so that we can use max heap later on them
         vector<vector<int>> dist(n, vector<int>(n, -1));
         queue<pair<int, int>> q;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
                     q.push({i, j});
                     dist[i][j] = 0;
                 }
@@ -15,10 +16,10 @@ public:
         }
         int dx[] = {1, -1, 0, 0};
         int dy[] = {0, 0, 1, -1};
-        while (!q.empty()) {
+        while(!q.empty()){
             auto [r, c] = q.front();
             q.pop();
-            for (int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++){
                 int nr = r + dx[i];
                 int nc = c + dy[i];
                 if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] == -1) {
@@ -27,27 +28,30 @@ public:
                 }
             }
         }
+        // using max heap to get maximum safeness factor
         priority_queue<pair<int, pair<int, int>>> pq;
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        pq.push({dist[0][0], {0, 0}});
-        visited[0][0] = true;
-        while (!pq.empty()) {
+        vector<vector<int>> maxSafe(n, vector<int>(n, INT_MIN)); // store maximum safeness factor from (0,0) till current 
+        maxSafe[0][0] = dist[0][0];
+        pq.push({maxSafe[0][0], {0,0}}); // {safeness factor,{x,y}}
+        while(!pq.empty()){
             auto [sf, cell] = pq.top();
             int r = cell.first;
             int c = cell.second;
             pq.pop();
-            if (r == n - 1 && c == n - 1) {
-                return sf;
-            }
-            for (int i = 0; i < 4; i++) {
+            if(r == n-1 && c == n-1) return sf;
+            if(maxSafe[r][c] > sf) continue;
+            for(int i = 0; i < 4; i++){
                 int nr = r + dx[i];
                 int nc = c + dy[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    pq.push({min(sf, dist[nr][nc]), {nr, nc}});
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
+                    int nsf = min(dist[nr][nc], sf);
+                    if(nsf > maxSafe[nr][nc]){
+                        maxSafe[nr][nc] = nsf;
+                        pq.push({nsf,{nr,nc}});
+                    }
                 }
             }
         }
-        return 0;
+        return maxSafe[n-1][n-1];
     }
 };
