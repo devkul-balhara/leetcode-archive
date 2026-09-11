d0d08eadeafdbda19e2e84bfa574d4e19eec220e
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-09-11 13:25:57 +0530

    Time: 10 ms (39.19%) | Memory: 15.8 MB (64.58%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,28 +11,18 @@
  */
 class Solution {
 public:
-    pair<int,int> cal(TreeNode* node){
+    pair<int,int> cal(TreeNode* node, int& ans){
         if(node == NULL) return {0,0};
-        int sum = node->val, num = 1;
-        pair<int,int> pl = cal(node->left);
-        pair<int,int> pr = cal(node->right);
-        sum += pl.first+pr.first;
-        num += pl.second+pr.second;
+        pair<int,int> pl = cal(node->left, ans);
+        pair<int,int> pr = cal(node->right, ans);
+        int sum = node->val+pl.first+pr.first, num = 1+pl.second+pr.second;
+        if((sum == 0 && num == 0) || (num != 0 && node->val == sum/num)) ans++;
         return {sum,num};
-    }
-    
-    int find(TreeNode* node){
-        if(node == NULL) return 0;
-        int cnt = 0;
-        pair<int,int> p = cal(node);
-        int sum = p.first, num = p.second;
-        if((sum == 0 && num == 0) || (num != 0 && node->val == sum/num)) cnt++;
-        cnt += find(node->left);
-        cnt += find(node->right);
-        return cnt;
     }
 
     int averageOfSubtree(TreeNode* root) {
-        return find(root);
+        int ans = 0;
+        cal(root, ans);
+        return ans;
     }
 };
