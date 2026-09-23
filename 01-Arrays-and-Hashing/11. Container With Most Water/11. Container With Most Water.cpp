cb8af0f0e8b62b1857f0af43a164bd624ab3a829
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-09-23 20:44:37 +0530

    [11] [Time Beats: 100.00%] [Memory Beats: 10.83%] - Leet2Hub

--- 01-Arrays-and-Hashing/11. Container With Most Water/11. Container With Most Water.cpp
+++ 01-Arrays-and-Hashing/11. Container With Most Water/11. Container With Most Water.cpp
@@ -1,1 +1,17 @@
-class Solution {public:    int maxArea(vector<int>& height) {        int i = 0, j = height.size()-1;        int ans = 0;        while(i < j){            ans = max(ans,(j-i)*min(height[j],height[i]));            if(height[i] < height[j]) i++;            else j--;        }        return ans;    }};// maximise the area of rectangle 
+1class Solution {
+2public:
+3    int maxArea(vector<int>& height) {
+4        int i = 0, j = height.size()-1;
+5        int ans = 0;
+6        while(i < j){
+7            ans = max(ans,(j-i)*min(height[j],height[i]));
+8            if(height[i] < height[j]) i++;
+9            else j--;
+10        }
+11        return ans;
+12    }
+13};
+14
+15// maximise the area of rectangle 
+16// area: (j-i) * min(height[j],height[i]) such that j > i
+17// for maximising (j-i) start from extreme end and then keep the max height 
