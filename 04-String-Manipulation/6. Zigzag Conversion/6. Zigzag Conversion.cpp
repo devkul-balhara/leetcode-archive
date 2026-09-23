712bdd828668a8ec20856a883c8b564be35f7000
Devkul Balhara <devkulbalharaofficial@gmail.com>
2026-09-23 20:45:07 +0530

    [6] [Time Beats: 50.66%] [Memory Beats: 70.87%] - Leet2Hub

--- 04-String-Manipulation/6. Zigzag Conversion/6. Zigzag Conversion.cpp
+++ 04-String-Manipulation/6. Zigzag Conversion/6. Zigzag Conversion.cpp
@@ -1,1 +1,18 @@
-        bool goingDown = true;        for(char c : s){            rows[currRow] += c;            if(currRow == 0) goingDown = true;            else if(currRow == numRows-1) goingDown = false;            currRow += goingDown ? 1 : -1;        }        string ans;        for(string &row : rows) ans += row;        return ans;    }        int currRow = 0;        vector<string> rows(numRows);        if(numRows == 1 || numRows >= s.size()) return s;    string convert(string s, int numRows) {public:
+1class Solution {
+2public:
+3    string convert(string s, int numRows) {
+4        if(numRows == 1 || numRows >= s.size()) return s;
+5        vector<string> rows(numRows);
+6        int currRow = 0;
+7        bool goingDown = true;
+8        for(char c : s){
+9            rows[currRow] += c;
+10            if(currRow == 0) goingDown = true;
+11            else if(currRow == numRows-1) goingDown = false;
+12            currRow += goingDown ? 1 : -1;
+13        }
+14        string ans;
+15        for(string &row : rows) ans += row;
+16        return ans;
+17    }
+18};
